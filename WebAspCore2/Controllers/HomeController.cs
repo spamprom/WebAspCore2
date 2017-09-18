29d0ae5320386a372d7b2bb8dbe10c0b01a1df43
Leonid Shulga <shulga@leonid.org.ua>
2017-09-18 17:53:07 +0300

    SignInManager

--- WebAspCore2/Controllers/HomeController.cs
+++ WebAspCore2/Controllers/HomeController.cs
@@ -16,13 +16,32 @@ namespace WebAspCore2.Controllers
     {
         private readonly MoneyService moneyService;
         private readonly UserManager<ApplicationUser> userManager;
-        public HomeController(MoneyService _moneyService, UserManager<ApplicationUser> _userManager)
+        private readonly IAuthorizationService authorizationService;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        public HomeController(MoneyService _moneyService, IAuthorizationService _authorizationService, UserManager<ApplicationUser> _userManager
+            , SignInManager<ApplicationUser> _signInManager)
         {
             moneyService = _moneyService;
             userManager = _userManager;
+            authorizationService = _authorizationService;
+            signInManager = _signInManager;
         }
         public IActionResult Index()
         {
+            //var userID = userManager.GetUserId(User);
+            //var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+            //await userManager.CreateAsync(user, model.Password);
+            //await _signInManager.SignInAsync(user, isPersistent: false);
+
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+            //var result = await _signInManager.PasswordSignInAsync(model.Email,
+            //    model.Password, model.RememberMe, lockoutOnFailure: false);
+            //if (result.Succeeded)
+
+            //await _signInManager.SignOutAsync();
+
             return View();
         }
 
