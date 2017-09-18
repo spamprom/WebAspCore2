cbf634bcd0cfe350f6e13f3126993ababde24746
Leonid Shulga <shulga@leonid.org.ua>
2017-09-18 17:22:56 +0300

    inject UserManager

--- WebAspCore2/Controllers/HomeController.cs
+++ WebAspCore2/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebAspCore2.BL;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using WebAspCore2.Data.IdentityModel;
 
 namespace WebAspCore2.Controllers
 {
@@ -13,9 +15,11 @@ namespace WebAspCore2.Controllers
     public class HomeController : Controller
     {
         private readonly MoneyService moneyService;
-        public HomeController(MoneyService _moneyService)
+        private readonly UserManager<ApplicationUser> userManager;
+        public HomeController(MoneyService _moneyService, UserManager<ApplicationUser> _userManager)
         {
             moneyService = _moneyService;
+            userManager = _userManager;
         }
         public IActionResult Index()
         {
